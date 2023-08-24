d637dfc803641564e0151192f24159afa0cf9fef
KJA000 <82318174+KJA000@users.noreply.github.com>
2023-08-24 18:07:38 +0900

    Update ft_print_comb.c

--- ft_print_comb.c
+++ ft_print_comb.c
@@ -1,24 +1,30 @@
 #include <unistd.h>
-void ft_print_comb(void);
 
-void ft_print_comb(void){
-   int i=0,j=0,k=0;
-   char a='0', b='1', c='2', sum;
-   while(i<8)
-	   a= a+b+c;
-       write(1,&sum,1);
-       while(j<9){
-	      while(k<10){
-		     c+=1;
-			 k+=1;
-		  } 
-		  b+=1;
-		  c+=1;
-		  j+=1
-	   }  
-	  a+=1;
-	  i+=1; 
+void ft_print_comb(void) {
+    char a = '0';
+    char b, c;
+
+    while (a <= '7') {
+        b = a + 1;
+        while (b <= '8') {
+            c = b + 1;
+            while (c <= '9') {
+                write(1, &a, 1);
+                write(1, &b, 1);
+                write(1, &c, 1);
+
+                if (a != '7' || b != '8' || c != '9') {
+                    write(1, ", ", 2);
+                }
+                c++;
+            }
+            b++;
+        }
+        a++;
+    }
 }
-int main(){
-   ft_print_comb();
+
+int main() {
+    ft_print_comb();
+    return 0;
 }
