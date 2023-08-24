fa1a664a02b09ab9fef808504f98fe3f2fd3a735
KJA000 <82318174+KJA000@users.noreply.github.com>
2023-08-24 21:23:03 +0900

    Update ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,15 +1,34 @@
 #include <unistd.h>
-void ft_putnbr(int nb);
 
-void ft_putnbr(int nb)
-{ 
-	char NB = (char)nb;
-	write(1,&NB,1);
+void ft_putchar(char c) 
+{
+    write(1, &c, 1);
 }
 
-int main()
-{ 
-	ft_putnbr(42);
-	
-	ft_putnbr(10000);
+void ft_putnbr(int nb) 
+{
+    if (nb < 0) 
+    {
+        ft_putchar('-');
+        nb = -nb;
+    }
+
+    if (nb >= 10) 
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    } 
+    else 
+    {
+        ft_putchar(nb + '0');
+    }
+}
+
+int main() 
+{
+    ft_putnbr(42);
+    ft_putchar('\n');  // newline for clarity
+    ft_putnbr(10000);
+    ft_putchar('\n');
+    return 0;
 }
